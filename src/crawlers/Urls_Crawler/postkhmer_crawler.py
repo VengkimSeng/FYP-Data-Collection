baa0303c971a1ae5bd23e77132cc148740aae8f9
Vengkim Seng <vengkim.s@gmail.com>
2025-03-13 21:42:14 +0700

    Remove file logging for postkhmer_crawler to prevent log file creation

--- src/crawlers/Urls_Crawler/postkhmer_crawler.py
+++ src/crawlers/Urls_Crawler/postkhmer_crawler.py
@@ -19,7 +19,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("postkhmer_scraper.log"),
         logging.StreamHandler()
     ]
 )
